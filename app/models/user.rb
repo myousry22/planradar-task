89b724a2c05080c16a4174d98f4f0e9de7735841
yousry <yousry@zetaton.com>
2024-11-21 20:30:48 +0200

    add user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ApplicationRecord
+
+
+  # Model Validations
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :send_due_date_reminder, inclusion: { in: [true, false] }
+  validates :due_date_reminder_interval, presence: true, inclusion: { in: 0..1 }
+  validates :time_zone, presence: true, inclusion: { in: ActiveSupport::TimeZone.all.map(&:name), message: "%{value} is not a valid time zone" }
+  validates :due_date_reminder_time, presence: true, if: :send_due_date_reminder?
+
+  # Model Associations
   has_many :tickets, foreign_key: 'assigned_user_id'
 
   def due_date_reminders_enabled?
